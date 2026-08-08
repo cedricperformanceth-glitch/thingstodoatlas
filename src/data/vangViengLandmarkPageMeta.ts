71379840bd83766116092e688e710b576b81f745
cedricperformanceth-glitch <cedric.performance.th@gmail.com>
2026-08-08 16:23:20 +0700

    Add Vang Vieng landmark page relationships

---
+++ src/data/vangViengLandmarkPageMeta.ts
@@ -0,0 +1,19 @@
+export const vangViengLandmarkRelated: Record<string, string[]> = {
+  'hot-air-balloon-paramotor-vang-vieng': ['nam-xay-viewpoint', 'pha-ngern-silver-cliff-viewpoint', 'blue-lagoon-1-vang-vieng', 'tham-chang-cave'],
+  'num-bor-keo-cave': ['tham-khan-cave', 'blue-lagoon-1-vang-vieng', 'pha-ngern-silver-cliff-viewpoint', 'kaeng-nyui-waterfall'],
+  'blue-lagoon-1-vang-vieng': ['blue-lagoon-2-vang-vieng', 'blue-lagoon-3-vang-vieng', 'nam-xay-viewpoint', 'tham-chang-cave'],
+  'blue-lagoon-2-vang-vieng': ['blue-lagoon-1-vang-vieng', 'blue-lagoon-3-vang-vieng', 'vang-vieng-inter-park-lagoon-9', 'nam-xay-viewpoint'],
+  'vang-vieng-inter-park-lagoon-9': ['blue-lagoon-3-vang-vieng', 'blue-lagoon-2-vang-vieng', 'kaeng-nyui-waterfall', 'pha-ngern-silver-cliff-viewpoint'],
+  'pha-ngern-silver-cliff-viewpoint': ['nam-xay-viewpoint', 'phapoungkham-cave-viewpoint', 'blue-lagoon-1-vang-vieng', 'num-bor-keo-cave'],
+  'blue-lagoon-3-vang-vieng': ['blue-lagoon-2-vang-vieng', 'vang-vieng-inter-park-lagoon-9', 'nam-xay-viewpoint', 'pha-ngern-silver-cliff-viewpoint'],
+  'nam-xay-viewpoint': ['pha-ngern-silver-cliff-viewpoint', 'phapoungkham-cave-viewpoint', 'blue-lagoon-2-vang-vieng', 'hot-air-balloon-paramotor-vang-vieng'],
+  'phapoungkham-cave-viewpoint': ['nam-xay-viewpoint', 'pha-ngern-silver-cliff-viewpoint', 'blue-lagoon-3-vang-vieng', 'num-bor-keo-cave'],
+  'kaeng-nyui-waterfall': ['tham-chang-cave', 'vang-vieng-inter-park-lagoon-9', 'num-bor-keo-cave', 'vang-vieng-zipline'],
+  'adams-climbing-school': ['vang-vieng-zipline', 'pha-ngern-silver-cliff-viewpoint', 'nam-xay-viewpoint', 'phapoungkham-cave-viewpoint'],
+  'vang-vieng-zipline': ['tham-nam-water-cave', 'adams-climbing-school', 'blue-lagoon-2-vang-vieng', 'tham-chang-cave'],
+  'tham-khan-cave': ['num-bor-keo-cave', 'tham-chang-cave', 'pha-ngern-silver-cliff-viewpoint', 'blue-lagoon-1-vang-vieng'],
+  'tham-non-nang-fa-cave': ['tham-nam-water-cave', 'tham-chang-cave', 'num-bor-keo-cave', 'kaeng-nyui-waterfall'],
+  'tham-chang-cave': ['tham-nam-water-cave', 'tham-non-nang-fa-cave', 'blue-lagoon-1-vang-vieng', 'kaeng-nyui-waterfall'],
+  'tham-nam-water-cave': ['tham-non-nang-fa-cave', 'vang-vieng-zipline', 'tham-chang-cave', 'blue-lagoon-3-vang-vieng'],
+  'phou-bia-from-vang-vieng': ['hot-air-balloon-paramotor-vang-vieng', 'nam-xay-viewpoint', 'pha-ngern-silver-cliff-viewpoint', 'pha-pa-tou']
+};
